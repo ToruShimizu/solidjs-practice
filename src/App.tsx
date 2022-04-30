4c02f9210a4ab92f73652154fbaa92c7cbd9cfa1
ToruShimizu <nobbyz.t.nn@gmail.com>
2022-04-30 21:22:32 +0900

    feat: createSignalを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import type { Component } from 'solid-js';
+import { Component, createSignal } from 'solid-js';
 
 import logo from './logo.svg';
 import styles from './App.module.css';
 import Child from './components/Child';
 
 const App: Component = () => {
+  const [first, setFirst] = createSignal("JSON");
+  const [last, setLast] = createSignal("Bourne");
+
   return (
     <div class={styles.App}>
       <header class={styles.header}>
