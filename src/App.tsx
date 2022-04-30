7887871ef0b576a6333c64f41ef4f2cd8d4e6097
ToruShimizu <nobbyz.t.nn@gmail.com>
2022-04-30 21:22:43 +0900

    feat: createEffectを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Component, createSignal } from 'solid-js';
+import { Component, createEffect, createSignal } from 'solid-js';
 
 import logo from './logo.svg';
 import styles from './App.module.css';
@@ -8,6 +8,7 @@ const App: Component = () => {
   const [first, setFirst] = createSignal("JSON");
   const [last, setLast] = createSignal("Bourne");
 
+  createEffect(() => console.log(`${first()} ${last()}`));
   return (
     <div class={styles.App}>
       <header class={styles.header}>
