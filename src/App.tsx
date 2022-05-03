fff014d0fa4bede67e9b856f11f764ed6f6e7406
ToruShimizu <nobbyz.t.nn@gmail.com>
2022-05-03 16:20:58 +0900

    feat: Counterコンポーネントを追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { Component, createEffect, createSignal } from 'solid-js';
 import logo from './logo.svg';
 import styles from './App.module.css';
 import Child from './components/Child';
+import Counter from './components/Counter';
 
 const App: Component = () => {
   const [first, setFirst] = createSignal("JSON");
   const [last, setLast] = createSignal("Bourne");
 
+
+
   createEffect(() => console.log(`${first()} ${last()}`));
   return (
     <div class={styles.App}>
       <header class={styles.header}>
+        <Counter/>
         <Child/>
       </header>
     </div>
